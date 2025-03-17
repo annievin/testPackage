dd3ef1dff32df655b758595126d040109511b5d6
annievin <anna_vinovets@brown.edu>
2025-03-17 08:38:25 -0400

    final for OLS

---
+++ R/estimate_beta.r
@@ -0,0 +1,14 @@
+#' My first function
+#'
+#' @param a A numerical vector.
+#' @param b Also a numerical vector.
+#'
+#' @return A numerical vector of a + b * a.
+#' @export
+#'
+#' @examples
+#' myfunc(3, 5)
+estimate_beta <- function(y, X) {
+  beta_hat <- solve(t(X) %*% X) %*% t(X) %*% y
+  return(beta_hat)
+}
